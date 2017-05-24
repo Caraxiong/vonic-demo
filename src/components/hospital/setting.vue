075aaba1810076a81c87ccee841f1080f7e3b976
caraxiong <571982634@qq.com>
2017-05-24 21:42:52 +0800

    update

--- src/components/hospital/setting.vue
+++ src/components/hospital/setting.vue
@@ -14,28 +14,108 @@
 			  label="医院名称">
 			</von-input>
 		  </accordion-item>
-		  <accordion-item title="邀请同事成为管理员" content-height="110">
-		    <p>
-		      如果有一天我能够拥有一条渔船，
-		      我愿放下所有执着做个渔夫住在海边，
-		      每一个早晨我航行在晨曦的海面，
-		      每一个黄昏我遥望在无际的海云间。
-		      我会把思绪都消失在波涛里，
-		      让澎湃的心等待风雨后的平息。
-		    </p>
+		  <accordion-item title="邀请同事成为管理员" content-height="200">
+		   	<table>
+		      	<tr>
+			      	<th>微信昵称</th>
+			      	<th>真实姓名</th>
+			      	<th>操作</th>
+		      	</tr>
+		      	<tr>
+			      	<td>小小</td>
+			      	<td>张三</td>
+			      	<td>
+			      		<a href="javascript:void(0)">解绑</a>|
+			      		<a href="javascript:void(0)" @click="remark()">备注</a>
+		      		</td>
+		      	</tr>
+		      	<tr>
+			      	<td>小小</td>
+			      	<td>张三</td>
+			      	<td>
+			      		<a href="javascript:void(0)">解绑</a>|
+			      		<a href="javascript:void(0)" @click="remark()">备注</a>
+		      		</td>
+		      	</tr>
+		    </table>
 	  	</accordion-item>
-	  	 <accordion-item title="订餐时间设置" content-height="110">
-		    <p>
-		      如果有一天我能够拥有一条渔船，
-		      我愿放下所有执着做个渔夫住在海边，
-		      每一个早晨我航行在晨曦的海面，
-		      每一个黄昏我遥望在无际的海云间。
-		      我会把思绪都消失在波涛里，
-		      让澎湃的心等待风雨后的平息。
-		    </p>
+	  	 <accordion-item title="订餐时间设置" content-height="200">
+		    <table>
+		      	<tr>
+			      	<th></th>
+			      	<th>提醒订餐时间</th>
+			      	<th>预计送餐时间</th>
+			      	<th>操作</th>
+		      	</tr>
+		      	<tr>
+			      	<td>早餐</td>
+			      	<td>前一天：19:00</td>
+			      	<td>6:00 - 8:00</td>
+			      	<td>
+			      		<md-button class="button button-positive" @click.native="update()">修改</md-button>
+		      		</td>
+		      	</tr>
+		      	<tr>
+			      	<td>午餐</td>
+			      	<td>前一天：19:00</td>
+			      	<td>6:00 - 8:00</td>
+			      	<td>
+			      		<md-button class="button button-positive" @click.native="update()">修改</md-button>
+		      		</td>
+		      	</tr>
+		      	<tr>
+			      	<td>晚餐</td>
+			      	<td>前一天：19:00</td>
+			      	<td>6:00 - 8:00</td>
+		      		<td>
+			      		<md-button class="button button-positive" @click.native="update()">修改</md-button>
+		      		</td>
+		      	</tr>
+		    </table>
 	  	</accordion-item>
 	</accordion>
 </template>
 <script>
-  
+  	export default {
+	  	data() {
+	  		return {
+	  			name:'',
+	  			officename:'',
+	  			time:'',
+	  			realname:''
+	  		}
+	  	},
+	  	methods: {
+	  		remark() {
+	  			let options = {
+		          effect: 'scale',
+		          buttons: [
+		            {text: '确定'},
+		            {text: '取消'},
+		          ]
+		        }
+
+		        let popup = $popup.fromTemplate('<von-input type="text" v-model="realname" placeholder="备注真实姓名" ></von-input>', options)
+
+		        popup.show().then((buttonIndex) => {
+		          console.log(buttonIndex)
+		        })
+	  		},
+	  		update() {
+	  			let options = {
+		          effect: 'scale',
+		          buttons: [
+		            {text: '确定'},
+		            {text: '取消'},
+		          ]
+		        }
+
+		        let popup = $popup.fromTemplate('<von-input type="text" v-model="time" placeholder="请输入送餐时间" label="预计送餐时间"></von-input>', options)
+
+		        popup.show().then((buttonIndex) => {
+		          console.log(buttonIndex)
+		        })
+	  		}
+	  	}
+  	}
 </script>
